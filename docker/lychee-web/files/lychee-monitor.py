ab0c964f7f5c4ce80f1a26e2e3c2158fde7c5aed
davlloyd <davidlloyd0@gmail.com>
2018-07-10 06:38:47 +1000

    Wavefront Dashboard update

---
+++ docker/lychee-web/files/lychee-monitor.py
@@ -0,0 +1,9 @@
+# Create monitoring agent Wavefront compliant to collect
+# following metrics
+
+# Users connected app.lychee.users.connected
+# Photos total app.lychee.album.photos.count
+# Albums total app.lychee.album.count
+# Album Space Consumed app.lychee.album.disk.consumed
+# Album Space Available  app.lychee.album.disk.available
+# 
